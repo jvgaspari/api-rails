13456996d44d41c6e5369b9b646a79d0bf9e6432
José Victor Gaspari <josevictorgaspari@gmail.com>
2023-11-30 20:49:21 -0300

    feat: add kind and kind relations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,19 @@
 class Contact < ApplicationRecord
+  belongs_to :kind
+
   def author
     "Jackson Pires"
   end
 
+  def description_without_object
+    self.kind.description
+  end
+
   def as_json(options={})
-    super(methods: :author, root: true)
+  super(
+      root: true,
+      methods: [:author, :description_without_object],
+      include: {kind: { only: :description }}
+      )
   end
 end
